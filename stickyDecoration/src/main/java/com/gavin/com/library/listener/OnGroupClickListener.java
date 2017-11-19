ee1594407829cf466e295283ee8a6edb8f50b32b
gavin <437955114@qq.com>
2017-11-19 16:02:11 +0800

    注解

--- stickyDecoration/src/main/java/com/gavin/com/library/listener/OnGroupClickListener.java
+++ stickyDecoration/src/main/java/com/gavin/com/library/listener/OnGroupClickListener.java
@@ -2,8 +2,8 @@ package com.gavin.com.library.listener;
 
 /**
  * @author gavin
- * @date 17/11/18
- * @description 头部点击事件
+ * date 17/11/18
+ * description 头部点击事件
  */
 
 public interface OnGroupClickListener {
