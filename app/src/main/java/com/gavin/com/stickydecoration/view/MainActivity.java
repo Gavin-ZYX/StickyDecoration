128deb0c6d28b0e360c3473e12317be1b7062b5c
gavin <437955114@qq.com>
2018-03-19 15:44:57 +0800

    test

--- app/src/main/java/com/gavin/com/stickydecoration/view/MainActivity.java
+++ app/src/main/java/com/gavin/com/stickydecoration/view/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
     }
 
     public void toSticky(View view) {
